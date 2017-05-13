0946b2ac7ba45c113ae39bc10634fdecd5ac34ce
Michael Walloschke <michael.walloschke@axians.de>
2017-05-13 12:03:06 +0200

    Created InputModel with unique ID

---
+++ HackathonVinci2/HackathonVinci2/Models/InputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackathonVinci2.Models
+{
+    public class InputModel
+    {
+        public Guid Id { get; private set; }
+
+        InputModel()
+        {
+            this.Id = new Guid();
+        }
+    }
+}
