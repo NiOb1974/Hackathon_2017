cfffd061555454eafa0bfc2c2f5807779d94f380
Oradim <oliver.gropp@vinci-energies.net>
2017-05-13 21:57:08 +0200

    signalR added

--- HackathonVinci2/HackathonVinci2/Controllers/HomeController.cs
+++ HackathonVinci2/HackathonVinci2/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.SqlClient;
+using HackathonVinci2.Models;
+using HackathonVinci2.Fassade;
 
 namespace HackathonVinci2.Controllers
 {
---
+++ HackathonVinci2/HackathonVinci2/Hubs/ResultHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace HackathonVinci2.Hubs
+{
+    public class ResultHub : Hub
+    {
+        public void Result(string results)
+        {
+            Clients.All.addNewMessageToPage(results);
+        }
+    }
+}
